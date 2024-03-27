cea8b4b9126ddf2a323bdd35bcd4527e10e5ee48
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-28 00:04:49 +0100

    reload_poimlmlp

--- Controllers/auth_controller.js
+++ Controllers/auth_controller.js
@@ -2,14 +2,24 @@ import user from "../Models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET, JWT_EXPIRATION } from "../default.js";
+import nodemailer from "nodemailer";
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "ppay81755@gmail.com",
+    pass: "iawd rctp qwwi bsot",
+  },
+});
+
 export async function signUp(req, res) {
-  const { name, nickName, email, password, phone, role, speciality, address } = req.body;
+  const { name, nickName, email, password, phone, role, speciality, address } =
+    req.body;
 
   const verifUser = await user.findOne({ email: req.body.email });
   if (verifUser) {
     console.log("user already exists");
     res.status(403).send({ message: "User already exists !" });
-    return; 
+    return;
   }
 
   console.log("Success");
@@ -29,7 +39,14 @@ export async function signUp(req, res) {
   newUser.address = req.body.address;
 
   await newUser.save();
-
+  const mailOptions = {
+    from: "ranianadine.benhadjyoussef@esprit.tn",
+    to: newUser.email,
+    subject: "Inscription réussie",
+    html: `<p>Bonjour ${newUser.name},</p>
+           <p>Votre compte a été créé avec succès !</p>
+           <p>Vous pouvez maintenant vous connecter à l'application en utilisant ce lien : <a href="lien-de-votre-application">Lien de votre application</a></p>`,
+  };
   const payload = {
     _id: newUser._id,
     name: newUser.name,
@@ -50,6 +67,13 @@ export async function signUp(req, res) {
     token: token,
     statusCode: res.statusCode,
     message: "Logged in with success!",
+  });
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
   });
 
   console.log(token);
