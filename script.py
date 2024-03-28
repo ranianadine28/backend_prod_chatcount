a467dbcf37157fe14b577d98b7fbd723e0287654
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-28 16:09:26 +0100

    rel:m

--- script.py
+++ script.py
@@ -55,7 +55,7 @@ def load(csv):
     global labelsFEC, rowsFEC
     labelsFEC = []
     rowsFEC = []
-    uploads_folder = "uploads"
+    uploads_folder = "/uploads"
     filepath = os.path.join(uploads_folder, csv)
     with open(filepath, 'r', encoding='utf-8') as file:
         i = 0
@@ -89,7 +89,7 @@ else:
 #print (rowsFEC [0])
 
 #with open('MotsCles.csv', 'r') as file:
-with open('uploads/MotsCles.csv', 'r', encoding="utf-8") as file:
+with open('/uploads/MotsCles.csv', 'r', encoding="utf-8") as file:
     i = 0
     labels = []
     rows = []
@@ -129,7 +129,7 @@ with open('uploads/MotsCles.csv', 'r', encoding="utf-8") as file:
 
 synonymes = []
 #with open('Synonymes.csv', 'r') as file:
-with open('uploads/Synonymes.csv', 'r', encoding="utf-8") as file:
+with open('/uploads/Synonymes.csv', 'r', encoding="utf-8") as file:
     i = 0
     for row in file:
         result = []
