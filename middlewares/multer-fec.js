244111835fd6a5a7eb4a796a769e87a06bfe4d81
rania <ranianadine.benhadjyoussef@esprit.tn>
2024-03-28 16:02:13 +0100

    rel:

--- middlewares/multer-fec.js
+++ middlewares/multer-fec.js
@@ -1,27 +1,18 @@
-import multer from "multer";
-import { v4 as uuidv4 } from "uuid";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 
-// Obtenez le chemin du répertoire du fichier JavaScript principal
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Définissez la configuration de stockage Multer
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    // Utilisez le même répertoire que celui où se trouve le fichier JavaScript principal
-    callback(null, __dirname);
+    callback(null, '/uploads'); 
   },
   filename: function (req, file, cb) {
-    // Utilisez le nom d'origine du fichier
-    cb(null, file.originalname);
+    cb(null, uuidv4());
   },
 });
 
-// Configurez Multer avec les paramètres de stockage
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 100000000 }, // Limite la taille du fichier à 100 MB
+  limits: { fileSize: 100000000 },
   fileFilter: (req, file, cb) => {
     // Filtrer les fichiers par extension
     if (!file.originalname.match(/\.(csv)$/)) {
@@ -29,6 +20,6 @@ const upload = multer({
     }
     cb(null, true);
   },
-}).single("csvFile");
+}).single('csvFile');
 
 export default upload;
